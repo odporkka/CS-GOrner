c0d2e5ae37773b48ea877a3ff665832f6067b425
Otto Porkka <ode.porkka@gmail.com>
2020-10-10 13:07:44 +0300

    Add mising TacticsMapPanel

---
+++ src/components/navigation/TacticsMapPanel.jsx
@@ -0,0 +1,71 @@
+import React, {useContext} from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Accordion from '@material-ui/core/Accordion'
+import AccordionSummary from '@material-ui/core/AccordionSummary'
+import AccordionDetails from '@material-ui/core/AccordionDetails'
+import Grid from '@material-ui/core/Grid'
+import SearchIcon from '@material-ui/icons/Search'
+import Typography from '@material-ui/core/Typography'
+
+// Own classes/compontets
+import { Context } from '../../context/Context'
+import MapCard from '../content/MapCard'
+
+// MUI styles
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: '100%',
+        marginBottom: '20px'
+    },
+    panel: {
+        // fontSize: theme.typography.pxToRem(15),
+        // fontWeight: theme.typography.fontWeightRegular,
+        backgroundColor: theme.palette.primary.light,
+    },
+    heading: {}
+}))
+
+
+
+/**
+ * Map search panel in tactics browse view.
+ *
+ * @return {JSX.Element}
+ * @constructor
+ */
+const TacticsMapPanel = (props) => {
+    const {
+        searchByMap
+    } = props
+    const classes = useStyles()
+    const { contentData } = useContext(Context)
+
+
+    return (
+        <div className={classes.root}>
+            <Accordion className={classes.panel}>
+                <AccordionSummary
+                    expandIcon={<SearchIcon />}
+                    aria-controls="search settings"
+                    id="panel1a-header"
+                >
+                    <Typography className={classes.heading} variant="h6">Tactics by map</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+
+                    <Grid container spacing={2} >
+                        { contentData.maps.map((map) => (
+                                <Grid item key={map.id} xs={12}>
+                                    <MapCard map={map} key={map.id} searchByMap={searchByMap}/>
+                                </Grid>
+                            )
+                        )}
+                    </Grid>
+
+                </AccordionDetails>
+            </Accordion>
+        </div>
+    );
+}
+
+export default TacticsMapPanel
