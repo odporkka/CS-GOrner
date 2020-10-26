eed7e1af7465e16d1cce418a446f5874125b5c71
Otto Porkka <ode.porkka@gmail.com>
2020-10-26 02:53:55 +0200

    Post numbering on front page

--- src/components/content/NewPosts.jsx
+++ src/components/content/NewPosts.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
 import Container from '@material-ui/core/Container'
 import Grid from '@material-ui/core/Grid'
 import Paper from '@material-ui/core/Paper'
@@ -7,7 +8,13 @@ import Typography from '@material-ui/core/Typography'
 // Own classes/components
 import PostTeaser from './PostTeaser'
 
-
+// MUI styles
+const useStyles = makeStyles({
+    postNumber: {
+        paddingBottom: '10px',
+        textAlign: 'right'
+    }
+})
 
 /**
  * Top X new posts component in home page.
@@ -20,20 +27,22 @@ const NewPosts = (props) => {
     const {
         posts
     } = props
-    
-    
+    const classes = useStyles()
+
+
     return (
         <Paper>
             <Container>
-                <Grid container spacing={4}>
+                <Grid container spacing={2}>
                     <Grid item xs={12}>
                         <Typography variant='h5'>New Posts</Typography>
                     </Grid>
 
-                    { posts.map((post) => (
-                            <Grid item key={post.id} xs={12}>
-                                <PostTeaser postData={post} />
-                            </Grid>
+                    { posts.map((post, i) => (
+                        <Grid key={post.id} item xs={12}>
+                            <Typography className={classes.postNumber}>{`#${i + 1}`}</Typography>
+                            <PostTeaser postData={post} />
+                        </Grid>
                         )
                     )}
                 </Grid>
