6793d6c7cd48051b18cf008874d771746bc91e10
Otto Porkka <otto.porkka@iki.fi>
2021-01-15 05:17:28 +0200

    Fixed full post post fetch with id

--- src/components/views/TacticPage.jsx
+++ src/components/views/TacticPage.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 import { makeStyles } from '@material-ui/core/styles'
 import Button from '@material-ui/core/Button'
 import EditIcon from '@material-ui/icons/Edit'
@@ -9,6 +9,7 @@ import Grid from '@material-ui/core/Grid'
 import { AWSCognitoUserContext } from '../../context/AWSCognitoUserContext'
 import { Context } from '../../context/Context'
 import Post from '../content/Post'
+import * as api from "../../backend/api";
 
 // MUI styles
 const useStyles = makeStyles((theme) => ({
@@ -32,16 +33,32 @@ const TacticPage = (props) => {
     const classes = useStyles()
     // Actually id for now. TODO: rename in Router?
     const title = props.match.params.title
+    const history = useHistory()
     const { contentData } = useContext(Context)
     const { AWSCognitoUser } = useContext(AWSCognitoUserContext)
     const [ post, setPost ] = useState(null)
 
     useEffect(() => {
-        const postData = contentData.newPosts.find((p) => (p.id === title))
-        if (postData) {
-            setPost(postData)
+        let mounted = true
+        const fetch = async () => {
+            const response = await api.fetchPostWithId(title)
+            if (mounted) {
+                if (response) {
+                    if (!response.error) {
+                        setPost(response)
+                    } else {
+                        alert(`Error while fetching post with id ${title}!`)
+                    }
+                } else {
+                    alert(`Post with id ${title} not found! It might been deleted recently!`)
+                    history.push('/tactics')
+                }
+            }
         }
-    }, [contentData, title])
+        fetch().catch((e) => console.log(e))
+
+        return () => { mounted = false }
+    }, [title])
 
 
     return (
