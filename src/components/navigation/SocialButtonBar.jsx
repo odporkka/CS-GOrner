13f84596c2632ff13b5ad922f4271b299c7937b8
Otto Porkka <otto.porkka@iki.fi>
2021-01-26 18:29:55 +0200

    Added consent on login

--- src/components/navigation/SocialButtonBar.jsx
+++ src/components/navigation/SocialButtonBar.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
 import Grid from '@material-ui/core/Grid'
 import IconButton from '@material-ui/core/IconButton'
 import LinkedInIcon from '@material-ui/icons/LinkedIn'
-import InstagramIcon from '@material-ui/icons/Instagram'
-import FacebookIcon from '@material-ui/icons/Facebook'
+// import InstagramIcon from '@material-ui/icons/Instagram'
+// import FacebookIcon from '@material-ui/icons/Facebook'
 import SportsEsportsIcon from '@material-ui/icons/SportsEsports'
 // TODO: Icons for steam and discord
 
-
+const useStyles = makeStyles((theme) => ({
+    socialIcon: {
+        width: '24px',
+        height: '24px'
+    },
+}))
 
 /**
  * Social buttons in Footer component.
@@ -16,11 +22,12 @@ import SportsEsportsIcon from '@material-ui/icons/SportsEsports'
  * @constructor
  */
 const SocialButtonBar = () => {
+    const classes = useStyles()
+    const discordLink = 'https://discord.gg/bkdBypPRq2'
     const linkedInLink = 'https://www.linkedin.com/in/odporkka/'
-    const instagramLink = 'http://example.com'
-    const facebookLink = 'http://example.com'
+    // const instagramLink = 'http://example.com'
+    // const facebookLink = 'http://example.com'
     const steamGroupLink = 'http://example.com'
-    const discordLink = 'http://example.com'
 
 
     return (
@@ -28,7 +35,7 @@ const SocialButtonBar = () => {
             <Grid item>
                 <IconButton aria-label='discord' color='inherit'
                     onClick={() => (window.open(discordLink))}>
-                    <SportsEsportsIcon  />
+                    <img src='./images/discord.png' alt='discord-server-link' className={classes.socialIcon}/>
                 </IconButton>
             </Grid>
             <Grid item>
@@ -37,18 +44,18 @@ const SocialButtonBar = () => {
                     <SportsEsportsIcon/>
                 </IconButton>
             </Grid>
-            <Grid item>
-                <IconButton aria-label='facebook' color='inherit'
-                    onClick={() => (window.open(facebookLink))} >
-                    <FacebookIcon />
-                </IconButton>
-            </Grid>
-            <Grid item>
-                <IconButton aria-label='instagram' color='inherit'
-                    onClick={() => (window.open(instagramLink))} >
-                    <InstagramIcon />
-                </IconButton>
-            </Grid>
+            {/*<Grid item>*/}
+            {/*    <IconButton aria-label='facebook' color='inherit'*/}
+            {/*        onClick={() => (window.open(facebookLink))} >*/}
+            {/*        <FacebookIcon />*/}
+            {/*    </IconButton>*/}
+            {/*</Grid>*/}
+            {/*<Grid item>*/}
+            {/*    <IconButton aria-label='instagram' color='inherit'*/}
+            {/*        onClick={() => (window.open(instagramLink))} >*/}
+            {/*        <InstagramIcon />*/}
+            {/*    </IconButton>*/}
+            {/*</Grid>*/}
             <Grid item>
                 <IconButton aria-label='linked in' color='inherit'
                     onClick={() => (window.open(linkedInLink))} >
--- src/components/views/SteamLoginPage.jsx
+++ src/components/views/SteamLoginPage.jsx
@@ -9,11 +9,13 @@ import Button from '@material-ui/core/Button'
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import CardMedia from '@material-ui/core/CardMedia'
+import CheckBox from "@material-ui/core/Checkbox"
 
 // Own classes/components
 import { SteamUserContext } from '../../context/SteamUserContext'
 import LoadingSpinner from '../content/LoadingSpinner'
 import * as steamService from '../../util/steamService'
+import {FormControlLabel, withStyles} from "@material-ui/core";
 
 
 
@@ -24,7 +26,7 @@ const useStyles = makeStyles((theme) => ({
     },
     cardDiv: {
         maxWidth: '600px',
-        padding: '50px'
+        padding: '25px'
     },
     card: {
         border: `2px solid ${theme.palette.secondary.main}`,
@@ -48,6 +50,15 @@ const useStyles = makeStyles((theme) => ({
     }
 }))
 
+const ConsentCheckBox = withStyles({
+    root: {
+        color: '#ffffff',
+        '&$checked': {
+            color: '#5d79ae'
+        }
+    }
+})((props) => <CheckBox color='default' {...props}/>)
+
 
 /**
  * '/admin' -page used for logging in as AWS IAM user (editors).
@@ -63,6 +74,7 @@ const SteamLoginPage = () => {
     const classes = useStyles()
     const history = useHistory()
     const [ loading, setLoading ] = useState(true)
+    const [ consentGiven, setConsentGiven ] = useState(false)
     const { steamUser, setSteamUser } = useContext(SteamUserContext)
 
     useEffect(() => {
@@ -103,51 +115,63 @@ const SteamLoginPage = () => {
                     } 
 
                     { !steamUser && !loading &&
-                    <>
-                        <Grid item xs={12} container justify='center'>
-                            <Grid item xs={12} className={classes.cardDiv}>
-                                {/*<Card classes={{root: classes.card}}>*/}
-                                <Card className={classes.card}>
-                                    <CardContent>
-                                        <Typography variant='h6' align='center'>
-                                            No login detected!
-                                        </Typography>
-                                        <Typography variant='body1'>
-                                            Log in is required to be able to vote on posts.
-                                            We only use your steamid and information from you public profile.
-                                            Login is valid for 12 hours after which you must log in again through steam.
-                                        </Typography>
-                                    </CardContent>
-                                </Card>
-                            </Grid>
-                            <Grid item xs={12}>
+                    <div>
+                        <Grid item xs={12} className={classes.cardDiv}>
+                            <Card className={classes.card}>
+                                <CardContent>
+                                    <Typography variant='h6' align='center'>
+                                        No login detected!
+                                    </Typography>
+                                    <Typography variant='body1'>
+                                        Log in is required to be able to vote on posts.
+                                        We only use your steamid and information from you public profile.
+                                        Login expires after being inactive for 12 hours after which you must
+                                        log in again through steam.
+                                    </Typography>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                        <Grid item xs={12} style={{textAlign: 'center'}}>
+                            <FormControlLabel control={
+                                <ConsentCheckBox
+                                    checked={consentGiven}
+                                    onChange={() => setConsentGiven(!consentGiven)}
+                                />
+                            }
+                            label='I understand'
+                            />
+                        </Grid>
+                        {consentGiven &&
+                        <div>
+                            <Grid item xs={12} style={{textAlign: 'center'}}>
                                 <Typography variant='h6' align='center'>
                                     You can log in via Steam by clicking link below:
                                 </Typography>
                             </Grid>
-                        </Grid>
-                        <Grid item>
-                            <form action='https://steamcommunity.com/openid/login' method='post'>
-                                <input type='hidden' name='openid.identity'
-                                       value='http://specs.openid.net/auth/2.0/identifier_select' />
-                                <input type='hidden' name='openid.claimed_id'
-                                       value='http://specs.openid.net/auth/2.0/identifier_select' />
-                                <input type='hidden' name='openid.ns' value='http://specs.openid.net/auth/2.0' />
-                                <input type='hidden' name='openid.mode' value='checkid_setup' />
-                                <input type='hidden' name='openid.realm' value='http://localhost:3000' />
-                                <input type='hidden' name='openid.return_to' value='http://localhost:3000/login' />
-                                <input
-                                    type='image'
-                                    alt='steam_login_button'
-                                    src='/images/steam_login.png'
-                                    className={classes.steamLoginButton}/>
-                            </form>
-                        </Grid>
-                    </>
+                            <Grid item style={{textAlign: 'center'}}>
+                                <form action='https://steamcommunity.com/openid/login' method='post'>
+                                    <input type='hidden' name='openid.identity'
+                                           value='http://specs.openid.net/auth/2.0/identifier_select'/>
+                                    <input type='hidden' name='openid.claimed_id'
+                                           value='http://specs.openid.net/auth/2.0/identifier_select'/>
+                                    <input type='hidden' name='openid.ns' value='http://specs.openid.net/auth/2.0'/>
+                                    <input type='hidden' name='openid.mode' value='checkid_setup'/>
+                                    <input type='hidden' name='openid.realm' value='http://localhost:3000'/>
+                                    <input type='hidden' name='openid.return_to' value='http://localhost:3000/login'/>
+                                    <input
+                                        type='image'
+                                        alt='steam_login_button'
+                                        src='/images/steam_login.png'
+                                        className={classes.steamLoginButton}/>
+                                </form>
+                            </Grid>
+                        </div>
+                        }
+                    </div>
                     }
 
                     { steamUser && !loading &&
-                    <>
+                    <div>
                         <Grid item xs={12} container justify='center'>
                             <Grid item xs={12} className={classes.cardDiv}>
                                 {/*<Card classes={{root: classes.card}}>*/}
@@ -182,8 +206,7 @@ const SteamLoginPage = () => {
                                 </Button>
                             </Grid>
                         </Grid>
-                    </>
-
+                    </div>
                     }
                 </Grid>
             </Container>
